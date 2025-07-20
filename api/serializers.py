dda0a84861f5f98e9809c241289779d60fc5d15c
Edward Salkeld <edsalkeld@alcachofa.faith>
2025-07-20 12:14:48 +0100

    Fix API issues

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from places.models import Business
+from places.models import Business, BusinessGroup
+
+class BusinessGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BusinessGroup
+        fields = ['name']
 
 class BusinessSerializer(serializers.ModelSerializer):
+    business_group = BusinessGroupSerializer()
+
     class Meta:
         model = Business
-        fields = '__all__'
+        fields = [
+            'id', 'name', 'business_group', 'category', 'latitude', 'longitude',
+            'date_opened', 'date_closed', 'notes'
+        ]
--- city_list_project/settings.py
+++ city_list_project/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local apps
     'pages',
     'places',
+    'api',
+    # Third-party apps
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -132,6 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MAP_CENTER_LAT = os.environ.get('MAP_CENTER_LAT', '53.8008')
 MAP_CENTER_LNG = os.environ.get('MAP_CENTER_LNG', '-1.5491')
 MAP_ZOOM_LEVEL = os.environ.get('MAP_ZOOM_LEVEL', '13')
+
 # DRF settings
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
