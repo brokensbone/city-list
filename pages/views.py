e76d9cac94c3e53f067aaf04d889c58b9b1df3b7
Edward Salkeld <edsalkeld@alcachofa.faith>
2025-07-20 11:52:47 +0100

    Add a map view

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from places.models import Business
 
 
 def home_page_view(request):
@@ -7,4 +9,12 @@ def home_page_view(request):
 
 def about_page_view(request):
     return HttpResponse("<h1>About us</h1>")
+
+
+def map_page_view(request):
+    business = Business.objects.first()
+    context = {
+        'business': business
+    }
+    return render(request, 'pages/map.html', context)
 
