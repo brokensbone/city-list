8637b9a79032dee635962e9e6815f4f29093a23f
Edward Salkeld <edsalkeld@alcachofa.faith>
2025-07-24 18:38:09 +0100

    New views for BusinessGroups

--- src/places/views.py
+++ src/places/views.py
@@ -1,6 +1,6 @@
 
 from django.views.generic import ListView, DetailView
-from .models import Business
+from .models import Business, BusinessGroup
 
 class BusinessListView(ListView):
     model = Business
@@ -12,3 +12,13 @@ class BusinessDetailView(DetailView):
     model = Business
     template_name = 'places/business_detail.html'
     context_object_name = 'business'
+
+class BusinessGroupDetailView(DetailView):
+    model = BusinessGroup
+    template_name = 'places/business_group_detail.html'
+    context_object_name = 'business_group'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['businesses'] = Business.objects.filter(business_group=self.object).order_by('name')
+        return context
