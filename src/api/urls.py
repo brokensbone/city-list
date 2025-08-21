f2be9c437d04113d3ca49b2ef425d2bc33c308e7
Edward Salkeld <edsalkeld@alcachofa.faith>
2025-08-21 22:01:18 +0100

    Add all imported places to the map!
    Clearly temporary...

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import StatusAPIView, BusinessListAPIView
+from .views import ImportedPlacesListAPIView, StatusAPIView, BusinessListAPIView
 
-app_name = 'api'
+app_name = "api"
 
 urlpatterns = [
-    path('status/', StatusAPIView.as_view(), name='status'),
-    path('businesses/', BusinessListAPIView.as_view(), name='business-list'),
+    path("status/", StatusAPIView.as_view(), name="status"),
+    path("businesses/", BusinessListAPIView.as_view(), name="business-list"),
+    path("imports/", ImportedPlacesListAPIView.as_view(), name="imported-places-list"),
 ]
